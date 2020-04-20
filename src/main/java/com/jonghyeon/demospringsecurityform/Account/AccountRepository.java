a9b02a14141490a54153c0c941209984034b5d73
jonghyeonkim <whdgus8219@naver.com>
2020-04-21 00:29:17 +0900

    security first commit

---
+++ src/main/java/com/jonghyeon/demospringsecurityform/Account/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.jonghyeon.demospringsecurityform.Account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface AccountRepository extends JpaRepository<Account,Integer> {
+    Account findByUsername(String username);
+}
---
+++ src/test/java/com/jonghyeon/demospringsecurityform/DemoSpringSecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonghyeon.demospringsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
