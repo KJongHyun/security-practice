ad4a864a7d81b1744d2a5417ef1115e045a20bc4
jonghyeonkim <whdgus8219@naver.com>
2020-04-21 22:15:27 +0900

    security first commit

---
+++ src/main/java/com/jonghyeon/demospringsecurityform/form/SampleService.java
@@ -0,0 +1,15 @@
+package com.jonghyeon.demospringsecurityform.form;
+
+import com.jonghyeon.demospringsecurityform.Account.Account;
+import com.jonghyeon.demospringsecurityform.Account.AccountContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    public void dashboard() {
+        Account account = AccountContext.getAccount();
+        System.out.println("==============");
+        System.out.println(account.getUsername());
+    }
+
+}
